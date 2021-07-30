92d5b6ded904d676685b6e5b845e11e3e6a94a16
J_C___ <chat@chatsumlin.com>
2021-07-30 15:54:27 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,17 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { ThemeProvider } from "@material-ui/styles";
-import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
-
-const theme = createMuiTheme({
-    palette: {
-        type: "dark"
-    }
-});
 
 ReactDOM.render(
-    <ThemeProvider theme={theme}>
+    <React.Fragment>
         <App/>
-    </ThemeProvider>,
+    </React.Fragment>,
     document.getElementById('root')
 );
