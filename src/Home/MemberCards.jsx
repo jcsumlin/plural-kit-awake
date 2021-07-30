98a79049a1a74d26d2577ef306cb8c88f5de5ead
J_C___ <chat@chatsumlin.com>
2021-07-30 16:09:32 -0400

    Update MemberCards.jsx

--- src/Home/MemberCards.jsx
+++ src/Home/MemberCards.jsx
@@ -8,7 +8,7 @@ class MemberCards extends Component {
         return (
             <Grid item xs={2}>
                 <div style={{backgroundImage: "url(\"" + this.props.member.avatar_url + "\")"}} className="member-card-image">
-                    <Typography component={'h5'} variant="h5">{this.props.member.display_name}></Typography>
+                    <Typography component={'h5'} variant="h5">{this.props.member.display_name ? this.props.member.display_name : this.props.member.name}</Typography>
                     <Typography component={'h6'} variant="h6">{this.props.member.pronouns}</Typography>
                 </div>
             </Grid>
